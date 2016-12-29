232f402375a73e106f889299533a686960d3e628
starkc1 <cms117@live.com>
2016-12-29 12:00:31 -0500

    routing test

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ var angularApp = angular.module('angularApp',['ngRoute']);
 angularApp.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
+        .when('Angular920.github.io', {
+            templateUrl: 'index.html'
+        })
         .when('Angular920.github.io/Models', {
             templateUrl: 'views/models.html'
         })
