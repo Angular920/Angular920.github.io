dee117ade00caaea08393d65b23c28ace90d22bd
starkc1 <cms117@live.com>
2016-12-29 12:45:04 -0500

    routing test

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@ angularApp.config(['$routeProvider', function($routeProvider) {
         .when('/', {
             templateUrl: 'index.html'
         })
-        .when('Angular920.github.io/Domains', {
+        .when('/Domains', {
             templateUrl: '<h1>Routing Test</h1>'
         })
-        .when('Angular920.github.io/Models', {
+        .when('/Models', {
             templateUrl: 'views/models.html'
         })
         .otherwise({
