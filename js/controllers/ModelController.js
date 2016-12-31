87c68b90629bc45bd0f7129a386fcbecd247e00e
starkc1 <cameronstark@StarkAir.local>
2016-12-31 11:42:58 -0500

    fixed url typo

--- js/controllers/ModelController.js
+++ js/controllers/ModelController.js
@@ -1,5 +1,5 @@
 app.controller('ModelController', function($scope, $http, $routeParams) {
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + $routeParams.key).then(function(response) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + $routeParams.key).then(function(response) {
         $scope.model = response.data;
     });
 });
