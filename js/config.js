40eed137e6c3010e9df80b9dc35ec89203b4a7d2
starkc1 <cameronstark@StarkAir.local>
2016-12-31 12:47:56 -0500

    changed config

--- js/config.js
+++ js/config.js
@@ -1,12 +1,6 @@
 app.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider
-        .when('/', {
-            templateUrl: '<div><h4>Testing</h4></div>'
-        })
-        .when('/Domains', {
-            templateUrl: 'views/domains.html'
-        })
         .when('/Models/:key', {
             templateUrl: 'views/models.html',
         })
