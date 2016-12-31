9c78ed3425eb11f40336874f814c38be4f3fb07f
starkc1 <cameronstark@StarkAir.local>
2016-12-31 13:45:21 -0500

    added new class

--- js/config.js
+++ js/config.js
@@ -4,6 +4,9 @@ app.config(['$routeProvider', function($routeProvider) {
         .when('/Models/:key', {
             templateUrl: 'views/models.html',
         })
+        .when('/Models/:key/:class', {
+            templateUrl: 'views/class.html'
+        })
         .otherwise({
             redirectTo: '/',
         });
---
+++ js/controllers/ClassController.js
@@ -0,0 +1,5 @@
+app.controller('ClassController', function($scope, $http,  $routeParams) {
+    $http.get("http.//dev/nuviot.com/metadata/dox/entity" + "/" + $routeParams.key + "/" + $routeParams.class).then(function(response) {
+        $scope.class = response.data;
+    })
+})
