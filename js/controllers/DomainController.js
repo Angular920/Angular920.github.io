c53192c2522d1fb55e3e90bfe70e3e7c34081398
starkc1 <cameronstark@StarkAir.local>
2017-01-04 16:03:13 -0500

    testing controller

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,5 +1,9 @@
 app.controller('DomainController', function($scope, $http) {
 
+    $scope.showModels = function(key) {
+        console.log(key);
+    }
+
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -1,9 +1,6 @@
 app.controller('ModelListController', function($scope, $http) {
 
-    $scope.showModels = function(key) {
-        console.log(key);
-    }
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + key).then(function(response) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" ).then(function(response) {
         $scope.model = response.data;
     });
 });
