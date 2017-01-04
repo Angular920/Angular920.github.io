0886d448bbea99ae4dcbecbfa1743ec57b8fcf4f
starkc1 <cameronstark@StarkAir.local>
2017-01-04 15:59:25 -0500

    testing controller

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -3,7 +3,7 @@ app.controller('ModelListController', function($scope, $http) {
     $scope.showModels = function(key) {
         console.log(key);
     }
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + domainKey).then(function(response) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + key).then(function(response) {
         $scope.model = response.data;
     });
 });
