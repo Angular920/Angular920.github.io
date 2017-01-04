d13148b68ace7a3cf9530c98e96223b72d28f078
starkc1 <cameronstark@StarkAir.local>
2017-01-04 13:16:20 -0500

    testing ng-click to controller

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -1,8 +1,8 @@
 app.controller('ModelListController', function($scope, $http) {
 
     $scope.showModels = function(property) {
-        
-        $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + property).then(function(response) {
+        var domainKey = property;
+        $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + domainKey).then(function(response) {
             $scope.model = response.data;
         });
     }
