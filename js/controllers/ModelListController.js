7a65ee03253f02c8c136af87086973bbb894ecad
starkc1 <cameronstark@StarkAir.local>
2017-01-04 12:33:09 -0500

    added ng-click function

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -1,5 +1,9 @@
-app.controller('ModelListController', function($scope, $http, $routeParams) {
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + $routeParams.key).then(function(response) {
-        $scope.model = response.data;
-    });
+app.controller('ModelListController', function($scope, $http) {
+
+    $scope.showModels = function(property) {
+        
+        $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + property).then(function(response) {
+            $scope.model = response.data;
+        });
+    }
 });
