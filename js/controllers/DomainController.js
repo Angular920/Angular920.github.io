6820385b3ec119ed9b3d977acc7f3288ad87fe4b
starkc1 <cameronstark@StarkAir.local>
2017-01-04 17:00:43 -0500

    reverted changed

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,7 +1,5 @@
 app.controller('DomainController', function($scope, $http) {
-
-    console.log(key)
-
+    
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
