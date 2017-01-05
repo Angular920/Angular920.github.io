dce1453d14e6851ac12fac874e4980dcfe9f6ec5
starkc1 <cameronstark@StarkAir.local>
2017-01-05 10:33:44 -0500

    removed stuff

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,13 +1,5 @@
 app.controller('DomainController', function($scope, $http, domainKeyService) {
 
-    $scope.ShowModels = function(key) {
-        domainKeyService.PrepPassKey(key);
-    };
-
-    $scope.$on('pass', function () {
-        console.log(KeyService.key)
-    })
-
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
