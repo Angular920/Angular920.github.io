503d3199c079bf0e7d2fe014c8c9d8bd31d9bf6a
starkc1 <cameronstark@StarkAir.local>
2017-01-05 11:05:29 -0500

    fixed controller?

--- js/controllers/ModelDetailController.js
+++ js/controllers/ModelDetailController.js
@@ -1,4 +1,4 @@
-app.controller('ModelDetailController', function($scope, $http, $routeParams) {
+app.controller('ModelDetailController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/entity" + "/" + "DeviceAdmin" + "/" + "Action").then(function(response) {
         $scope.details = response.data;
     })
