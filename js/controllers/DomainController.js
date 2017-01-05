23e79ed40020ce817a551092b6f7d01684dc0602
starkc1 <cameronstark@StarkAir.local>
2017-01-05 11:33:36 -0500

    reverted changes

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -3,12 +3,4 @@ app.controller('DomainController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
-    
-   app.controller('ModelListController', function($scope, $http) {
-        $scope.search = function(param) {
-            $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + param).then(function(response) {
-                $scope.model = response.data;
-            });
-        };
-   });
 });
