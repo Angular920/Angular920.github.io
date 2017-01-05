6880453644cd0e8c6736d8a7c0c89f8a1fefb249
starkc1 <cameronstark@StarkAir.local>
2017-01-05 11:27:37 -0500

    controller test

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -3,10 +3,12 @@ app.controller('DomainController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
-
-    $scope.search = function(param) {
-        $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + param).then(function(response) {
-            $scope.model = response.data;
-        });
-    };
+    
+   app.controller('ModelListController', function($scope, $http) {
+        $scope.search = function(param) {
+            $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + param).then(function(response) {
+                $scope.model = response.data;
+            });
+        };
+   });
 });
