bbc1ce7cca3f6453426a51a94e7ab938565b1562
starkc1 <cameronstark@StarkAir.local>
2017-01-05 10:58:48 -0500

    added hardcode for testing

--- js/controllers/ModelDetailController.js
+++ js/controllers/ModelDetailController.js
@@ -1,5 +1,5 @@
 app.controller('ModelDetailController', function($scope, $http, $routeParams) {
-    $http.get("http://dev.nuviot.com/metadata/dox/entity" + "/" + $routeParams.key + "/" + $routeParams.classname).then(function(response) {
+    $http.get("http://dev.nuviot.com/metadata/dox/entity" + "/" + "DeviceAdmin" + "/" + "Action").then(function(response) {
         $scope.details = response.data;
     })
 })
