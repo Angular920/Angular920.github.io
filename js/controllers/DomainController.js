6c9c813da2f331e75cb098ade442494da254ad62
starkc1 <cameronstark@StarkAir.local>
2017-01-05 11:25:28 -0500

    testing

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -3,4 +3,10 @@ app.controller('DomainController', function($scope, $http) {
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
     });
+
+    $scope.search = function(param) {
+        $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + param).then(function(response) {
+            $scope.model = response.data;
+        });
+    };
 });
