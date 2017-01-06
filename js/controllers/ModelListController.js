bd80a852295e0b18fabe36acf7a4e310aed62971
starkc1 <cameronstark@StarkAir.local>
2017-01-06 13:08:11 -0500

    changed url

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -1,6 +1,6 @@
 app.controller('ModelListController', function($scope, $http, mySharedService) {
 
-    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/").then(function(response) {
+    $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + "DeviceAdmin").then(function(response) {
         $scope.model = response.data;
     });
 
