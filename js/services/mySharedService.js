b04e432a851632223612bac62e6d4e03ed2cba0d
starkc1 <cameronstark@StarkAir.local>
2017-01-06 11:18:41 -0500

    test

--- js/services/mySharedService.js
+++ js/services/mySharedService.js
@@ -6,7 +6,7 @@ app.factory('mySharedService', function($rootScope) {
     sharedService.prepForBroadcast = function(key) {
         this.key = key;
         this.broadcastItem();
-        console.log(key);
+        console.log(this.key);
     };
 
     sharedService.broadcastItem = function() {
