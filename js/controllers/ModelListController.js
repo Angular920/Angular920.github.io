7d5cfc656bdab241aa409311199bbc22935b6342
starkc1 <cameronstark@StarkAir.local>
2017-01-06 10:38:56 -0500

    test console.log

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -2,8 +2,8 @@ app.controller('ModelListController', function($scope, $http, shareDataService) 
 
     $scope.key = shareDataService.getList();
 
-    console.log(URL);
     $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + $scope.key).then(function(response) {
         $scope.model = response.data;
+        console.log(URL);
     });
 });
