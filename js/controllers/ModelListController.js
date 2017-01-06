9524f1480ee17d13e36761951a8a0f15d1edf702
starkc1 <cameronstark@StarkAir.local>
2017-01-06 16:33:59 -0500

    added http.get

--- js/controllers/ModelListController.js
+++ js/controllers/ModelListController.js
@@ -3,6 +3,9 @@ app.controller('ModelListController', ['$scope', '$http', 'mySharedService', fun
         console.log("FOO");
         mySharedService.showModelList = function(key) {
             console.log("HERE IS MY KEY: " + key);
+            $http.get("http://dev.nuviot.com/metadata/dox/domains" + "/" + key).then(function(response) {
+                $scope.model = response.data;
+            })
         };
     };
 }]);
