766f1ade06e488e84ddee27c67c6ba74a33d80b3
starkc1 <cameronstark@StarkAir.local>
2017-01-06 13:22:11 -0500

    testing ng-click

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,4 +1,4 @@
-app.controller('DomainController', function($scope, $http, sharedService, mySharedService) {
+app.controller('DomainController', function($scope, $http, mySharedService) {
 
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
