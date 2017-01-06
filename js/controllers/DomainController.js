202dc92eeb5915446788d6bbac034536fd30e7c2
starkc1 <cameronstark@StarkAir.local>
2017-01-06 11:04:50 -0500

    test

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,12 +1,6 @@
 app.controller('DomainController', function($scope, $http, mySharedService) {
+
     $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
         $scope.info = response.data;
-    });
-
-    $scope.passKey = function(key) {
-        mySharedService.preForBroadcast(key);
-    };
-    $scope.$on('handleBroadcast', function() {
-        $scope.key = mySharedService.key;
     });
 });
