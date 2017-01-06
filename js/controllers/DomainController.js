7d176387abf23787951ff480fabec7c84d9213ae
starkc1 <cameronstark@StarkAir.local>
2017-01-06 11:49:34 -0500

    testing

--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,5 +1,9 @@
 app.controller('DomainController', function($scope, $http, mySharedService) {
 
+    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
+        $scope.info = response.data;
+    });
+
     $scope.clickMe = function() {
         console.log('Hello World');
     };
