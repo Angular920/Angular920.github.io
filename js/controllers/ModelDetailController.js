9c51c98059faa9b3d517becbb3824d1088e8cf19
starkc1 <cameronstark@StarkAir.local>
2017-01-06 17:55:20 -0500

    added http.get

--- js/controllers/ModelDetailController.js
+++ js/controllers/ModelDetailController.js
@@ -3,7 +3,9 @@ app.controller('ModelDetailController', ['$scope', '$http', 'mySharedService', f
         console.log("ModelDetail Loaded");
         mySharedService.showModelDetail = function(domainKey, className) {
             console.log("HERE IS MY KEY AND NAME: " + domainKey + " AND " + className);
-
+            $http.get("http://dev.nuviot.com/metadata/dox/entity" + "/" + domainKey + "/" + className).then(function(response) {
+                $scope.details = response.data;
+            });
         };
     };
 }]); 
