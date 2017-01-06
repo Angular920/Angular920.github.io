0bb5e1df67dee3fecf854b9b0f400102e3c71779
starkc1 <cameronstark@StarkAir.local>
2017-01-06 11:48:16 -0500

    testing ng-click

--- js/app.js
+++ js/app.js
@@ -1,8 +1,2 @@
 var app = angular.module('angularApp',[]);
 
-
-app.controller('TestCtrl', function($scope) {
-    $scope.clickMe = function() {
-        console.log('hello world');
-    }
-});
--- js/controllers/DomainController.js
+++ js/controllers/DomainController.js
@@ -1,16 +1,7 @@
 app.controller('DomainController', function($scope, $http, mySharedService) {
 
-    $http.get("http://dev.nuviot.com/metadata/dox/domains").then(function(response) {
-        $scope.info = response.data;
-    });
-
-    $scope.passKey = function(key) {
-        mySharedService.prepForBroadcast(key);
-        console.log(key);
+    $scope.clickMe = function() {
+        console.log('Hello World');
     };
-    $scope.$on('handleBroadcast', function() {
-        $scope.key = mySharedService.key;
-        console.log($scope.key);
-    });
 
 });
